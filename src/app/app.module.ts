792e9a9249eca169a5515a24ed428ca0d1a60767
neo2k16 <jcarmagedon@gmail.com>
2017-12-12 17:26:29 -0500

    jrcr_ang_jc_nggallery
    2017-12-12-17-26
    Angular. Curso. Jesús Conde. nggallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageListComponent } from './gallery/image-list/image-list.component';
+import { ImageComponent } from './gallery/image-list/image.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent, NavbarComponent, GalleryComponent, ImageListComponent, ImageComponent ],
+  imports: [ BrowserModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
